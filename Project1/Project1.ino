6b400ef64b8fcdf5fbd197a74b6e99214144556a
hamjoman <helloingman@naver.com>
2021-04-04 16:03:56 +0900

    ing

--- Project1/Project1.ino
+++ Project1/Project1.ino
@@ -13,11 +13,22 @@ byte rowPins[ROWS] = {10, 11, 12, 13};   // 가로줄 핀 배정
 byte colPins[COLS] = {9, 8, 7, 6};   // 세로줄 핀 배정
 
 Keypad Calculatorpad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+// 키패드 기본 설정
+
+
+int segpin[] = {0,1,2,3,4,5,14,15,16,17,18,19} // 4dig 7seg 핀 상수 지정
 
 
 
 void setup() {
   // put your setup code here, to run once:
+
+  for(int i=0; i<6; i++){
+    pinMode(segpin[i], OUTPUT);
+  }
+    for(int i=14; i<20; i++){
+    pinMode(segpin[i], OUTPUT);
+  } // 7seg 핀 OUTPUT 설정
 
 }
 
